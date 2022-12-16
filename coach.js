4af4676f58745e967797e088811e12046912d200
Aakash <aakashgaurav456@gmail.com>
2022-12-16 22:19:54 +0530

    Day3 Works

--- coach.js
+++ coach.js
@@ -1,4 +1,22 @@
-let btn=document.querySelector(".button");
+let btn=document.querySelector("#des");
 btn.addEventListener("click",function (){
     window.location.href="payment.html"
 });
+document.querySelector("#des2").addEventListener("click",function (){
+    window.location.href="payment.html"
+})
+document.querySelector("#des3").addEventListener("click",function (){
+    window.location.href="payment.html"
+})
+document.querySelector("#des4").addEventListener("click",function (){
+    window.location.href="payment.html"
+})
+document.querySelector("#des5").addEventListener("click",function (){
+    window.location.href="payment.html"
+})
+document.querySelector("#des6").addEventListener("click",function (){
+    window.location.href="payment.html"
+})
+document.querySelector("#last").addEventListener("click",function (){
+    window.location.href="payment.html"
+})
